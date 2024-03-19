a4856beae3929bdec4f8f62173087278e6b98547
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2024-03-19 16:36:26 +0100

    finish qr-generator wiht node.js

--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -1,5 +1,36 @@
-import inquirer from "inquirer"
-import qr-image from "qr-image"
+import inquirer from "inquirer";
+import qr from "qr-image";
+//var qr = require("qr-image"); Outdated
+import fs, { writeFile } from "fs";
+import { log } from "console";
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your URL:",
+      name: "URL",
+    },
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr-image.png"));
+    //console.log(answers);
+    // Use user feedback for... whatever!!
+
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
 /* 
 1. Use the inquirer npm package to get user input.
