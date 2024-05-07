7e774437a501cc5317954e3c4d5ec04ef8ece416
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2024-05-07 16:51:04 +0200

    Start lesson 3.4 Middleware

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -3,9 +3,20 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  console.log(req);
+  //console.log(req.rawHeaders);
+  res.send("<H1>Home</H1>");
 });
 
-app.listen(3000, () => {
+app.get("/about", (req, res) => {
+  res.send(`<H1>About</H1>
+  <p>This is about me</p>
+  `);
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<H1>Contact</H1>");
+});
+
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
